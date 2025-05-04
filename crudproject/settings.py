1bde9c882e9efa2a4daabe134a79f598c2e35aad
mark murangiri <91324441+markm334@users.noreply.github.com>
2025-05-04 12:50:40 +0300

    Update settings.py

--- crudproject/settings.py
+++ crudproject/settings.py
@@ -117,6 +117,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Your 'static' folder for development files
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
