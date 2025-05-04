ef821f6f08bcfefaf9a9e13ca2c9e9dcff13350e
mark murangiri <91324441+markm334@users.noreply.github.com>
2025-05-04 12:32:53 +0300

    Update settings.py

--- crudproject/settings.py
+++ crudproject/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['markmurangiri.pythonanywhere.com']
+
 
 # Application definition
 
