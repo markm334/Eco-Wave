bbf0454c9c594257e223be595ea7f5e98c75beee
mark murangiri <91324441+markm334@users.noreply.github.com>
2025-05-04 12:57:22 +0300

    Update urls.py

--- crudproject/urls.py
+++ crudproject/urls.py
@@ -6,4 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('crudapp.urls')),  # Directs root URL to your app
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
